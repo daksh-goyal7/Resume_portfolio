2f70172ab27a479c29841038d6348dae71fd75dc
daksh-goyal7 <149812584+daksh-goyal7@users.noreply.github.com>
2024-01-16 15:06:57 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+col1,col2=st.columns(2)
+with col1:
+    st.image("venv/images/daksh.jpg",width=600)
+with col2:
+    st.title("Daksh Goyal")
+    content=("Hi, I am Daksh. I am a python programmer and a student. I will graduate in 2025 from Thapar institute of Engineering"
+             " & Technology."
+             " I am currently pursuing my Bachelor's of Engineering degree in field of Electronics & Communication Engineering.")
+    st.info(content)
+content2="""Below you can find some of the apps I have built in python. Feel free to contact me."""
+st.write(content2)
+
+col3,empty_col,col4=st.columns([1.5,0.5,1.5])
+df=pandas.read_csv("venv/data.csv",sep=";")
+
+with col3:
+    for index,rows in df[:10].iterrows():
+        st.header(rows["title"])
+        st.write(rows["description"])
+        st.image("venv/images/"+rows["image"])
+        st.write("[Source Code](https://pythonhow.com)")
+
+with col4:
+    for index,rows in df[10:].iterrows():
+        st.header(rows["title"])
+        st.write(rows["description"])
+        st.image("venv/images/"+rows["image"])
+        st.write("[Source Code](https://pythonhow.com)")
